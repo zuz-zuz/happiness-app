2392df6fce4079e2a63551ebbc16b45ac6eca922
Zuz <pacutovazuzana@gmail.com>
2020-09-15 14:33:46 +0200

    update

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,9 +9,9 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import Header from './Header';
 import MainFeaturedPost from './MainFeaturedPost';
 import FeaturedPost from './FeaturedPost';
-import Main from './Main';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
+import './image1.JPG'
 
 const useStyles = makeStyles((theme) => ({
   mainGrid: {
@@ -23,53 +23,36 @@ const sections = [
   { title: 'Technology', url: '#' },
   { title: 'Design', url: '#' },
   { title: 'Culture', url: '#' },
-  { title: 'Business', url: '#' }
+  { title: 'Business', url: '#' },
 ];
 
 const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
+  title: 'Main picture',
   description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: 'https://source.unsplash.com/random',
+    "Something",
+  image: '/image1.JPG',
   imgText: 'main image description',
-  linkText: 'Continue reading…',
 };
 
 const featuredPosts = [
   {
     title: 'Featured post',
     date: 'Nov 12',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+    image: '/image1.JPG',
     imageText: 'Image Text',
   },
   {
     title: 'Post title',
     date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+    image: '/image1.JPG',
     imageText: 'Image Text',
   },
 ];
 
-const posts = {
-  post1: {name: 'Lalala'},
-  post2: {name: 'Papapa'},
-  post3: {name: 'Tratata'}
-};
-
 const sidebar = {
   title: 'About',
   description:
-    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
-  archives: [
-    { title: 'March 2020', url: '#' },
-    { title: 'February 2020', url: '#' },
-    { title: 'January 2020', url: '#' },
-    { title: 'November 1999', url: '#' }
-  ],
+    'This is what the project is about.',
   social: [
     { name: 'GitHub', icon: GitHubIcon },
     { name: 'Twitter', icon: TwitterIcon },
@@ -84,26 +67,25 @@ export default function App() {
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header title="Blog" sections={sections} />
+        <Header title="A PORTION OF HAPPINESS" sections={sections} />
         <main>
+          {/* namiesto MainFeaturedPost dam logo alebo obrazok */}
           <MainFeaturedPost post={mainFeaturedPost} />
+          {/* tu namiesto FeaturedPost dam random obrazky ako priklady co ti moze vyjst */}
           <Grid container spacing={4}>
-            {featuredPosts.map((post) => (
-              <FeaturedPost key={post.title} post={post} />
-            ))}
+            {featuredPosts.map((post) => <FeaturedPost key={post.title} post={post} />)}
           </Grid>
           <Grid container spacing={5} className={classes.mainGrid}>
-            <Main title="From the firehose" posts={posts} />
             <Sidebar
               title={sidebar.title}
               description={sidebar.description}
-              archives={sidebar.archives}
               social={sidebar.social}
             />
           </Grid>
         </main>
       </Container>
       <Footer title="Footer" description="Something here to give the footer a purpose!" />
+      <img src={''} alt={'Faro'} hight={500} />
     </React.Fragment>
   );
 }
