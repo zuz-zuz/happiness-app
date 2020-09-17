5d7b193c51d3ee4f3dcbe4a22c19f3dfe8656bcf
Zuz <pacutovazuzana@gmail.com>
2020-09-17 13:57:00 +0200

    new version update

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,91 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import TwitterIcon from '@material-ui/icons/Twitter';
 import Header from './Header';
-import MainFeaturedPost from './MainFeaturedPost';
-import FeaturedPost from './FeaturedPost';
-import Sidebar from './Sidebar';
-import Footer from './Footer';
-import './image1.JPG'
 
-const useStyles = makeStyles((theme) => ({
-  mainGrid: {
-    marginTop: theme.spacing(3),
-  },
-}));
-
-const sections = [
-  { title: 'Technology', url: '#' },
-  { title: 'Design', url: '#' },
-  { title: 'Culture', url: '#' },
-  { title: 'Business', url: '#' },
-];
-
-const mainFeaturedPost = {
-  title: 'Main picture',
-  description:
-    "Something",
-  image: '/image1.JPG',
-  imgText: 'main image description',
-};
-
-const featuredPosts = [
-  {
-    title: 'Featured post',
-    date: 'Nov 12',
-    image: '/image1.JPG',
-    imageText: 'Image Text',
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    image: '/image1.JPG',
-    imageText: 'Image Text',
-  },
-];
-
-const sidebar = {
-  title: 'About',
-  description:
-    'This is what the project is about.',
-  social: [
-    { name: 'GitHub', icon: GitHubIcon },
-    { name: 'Twitter', icon: TwitterIcon },
-    { name: 'Facebook', icon: FacebookIcon },
-  ],
-};
-
-export default function App() {
-  const classes = useStyles();
-
+const App = () => {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg">
-        <Header title="A PORTION OF HAPPINESS" sections={sections} />
-        <main>
-          {/* namiesto MainFeaturedPost dam logo alebo obrazok */}
-          <MainFeaturedPost post={mainFeaturedPost} />
-          {/* tu namiesto FeaturedPost dam random obrazky ako priklady co ti moze vyjst */}
-          <Grid container spacing={4}>
-            {featuredPosts.map((post) => <FeaturedPost key={post.title} post={post} />)}
-          </Grid>
-          <Grid container spacing={5} className={classes.mainGrid}>
-            <Sidebar
-              title={sidebar.title}
-              description={sidebar.description}
-              social={sidebar.social}
-            />
-          </Grid>
-        </main>
-      </Container>
-      <Footer title="Footer" description="Something here to give the footer a purpose!" />
-      <img src={''} alt={'Faro'} hight={500} />
-    </React.Fragment>
-  );
+  <>
+    <Header />
+  </>
+  )
 }
+
+export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import BeKind from '../Photos/BeKind2.jpg';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
+    borderBottom: '2px solid darkgrey',
   },
   toolbarTitle: {
     flex: 1,
@@ -25,50 +23,44 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Header(props) {
+const Header = () => {
   const classes = useStyles();
-  const { sections, title } = props;
 
   return (
-    <React.Fragment>
-      <Toolbar className={classes.toolbar}>
-        <Button size="small">Subscribe</Button>
-        <Typography
-          component="h2"
-          variant="h5"
-          color="inherit"
-          align="center"
-          noWrap
-          className={classes.toolbarTitle}
-        >
-          {title}
-        </Typography>
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-        <Button variant="outlined" size="small">
-          Sign up
-        </Button>
-      </Toolbar>
-      <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
-        {sections.map((section) => (
-          <Link
+    <Grid container style={{maxWidth: '80%', margin: 'auto'}} >
+      <Grid item xs={12}>
+        <Toolbar className={classes.toolbar}>
+          <Button size="small">SHARE WITH FRIENDS</Button>
+          <Typography
+            component="h2"
+            variant="h5"
             color="inherit"
+            align="center"
             noWrap
-            key={section.title}
-            variant="body2"
-            href={section.url}
-            className={classes.toolbarLink}
-          >
-            {section.title}
-          </Link>
-        ))}
-      </Toolbar>
-    </React.Fragment>
+            className={classes.toolbarTitle}
+            >
+            A PORTION OF HAPPINESS
+          </Typography>
+          <Button variant="outlined" size="small">
+            FOLLOW ON INSTAGRAM
+          </Button>
+       </Toolbar>
+      </Grid>
+      <Grid item xs={12}>
+        <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
+          <div>Today</div>
+          <div>Today</div>
+          <div>Today</div>
+          <div>Today</div>
+          <div>Today</div>
+          <div>Today</div>
+        </Toolbar>
+      </Grid>
+      <Grid item xs={12}>
+        <img alt='Logo' src={BeKind} style={{width: '100%'}} />
+      </Grid>
+    </Grid >
   );
 }
 
-Header.propTypes = {
-  sections: PropTypes.array,
-  title: PropTypes.string,
-};
+export default Header;
